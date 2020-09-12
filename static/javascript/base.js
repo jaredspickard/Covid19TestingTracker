82f36394d2119ca86b4e48143cdf8bf0cfafd777
Jared Spickard <jaredspickard@berkeley.edu>
2020-09-11 18:01:35 -0700

    in the process of restructuring files and switching to a new folder

--- static/javascript/base.js
+++
@@ -1,7 +0,0 @@
-// Get the page to select the appropriate list item (Home or Leaderboard)
-$(document).ready(function() {
-    // Get the hidden tag storing the representation of the selected page
-    var pageLinkID = document.getElementById("selectedLink").value;
-    // Set one of the list items to be active
-    document.getElementById(pageLinkID).className = "nav-item active";
-});
